bf41f1be9a725c04ba637ab14ee82ac42964ac6e
Staryui <jondellturner@gmail.com>
2018-09-06 20:11:27 -0700

    Changes image

---
+++ Self_portrait_folder/sketch.js
@@ -0,0 +1,37 @@
+function setup() {
+	createCanvas(640, 360);
+}
+
+function draw() {
+	background(220);
+	ellipse(320, 180, 300);
+    // self portrait
+function setup() {
+	createCanvas(640, 360);
+}
+    function setup() {
+	createCanvas(640, 360);
+}
+
+function draw() {
+	background("white");
+	noStroke();
+	
+	// face
+	fill("green");
+	rect(100, 50, 200, 250);
+	
+	// eyes
+	fill("blue");
+	ellipse(150, 150, 50, 100);
+	ellipse(250, 150, 40, 80);
+	
+	// ears
+	fill("yellow");
+	ellipse(150, 150, 40, 40);
+	ellipse(250, 150, 30, 30);
+
+	// mouth
+	stroke("blue");
+	line(150, 250, 250, 280);
+}
