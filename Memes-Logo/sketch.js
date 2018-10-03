bdf9a5d7c968f964e21f2c5f79f9ea3ed3c364b7
Staryui <jondellturner@gmail.com>
2018-10-02 20:21:55 -0700

    made a meme page

---
+++ Memes-Logo/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(500, 500);
+}
+
+function draw() {
+    background(0);
+    fill(255);
+    textAlign(CENTER, CENTER);
+    textSize(100);
+    textFont("Comic Sans MS");
+    text("BLACK GUY!!", width/2, height/2);
+}
