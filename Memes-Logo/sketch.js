fe9f143a855ecb48976a225447a0018e6f787124
Staryui <jondellturner@gmail.com>
2018-10-04 19:53:55 -0700

    Made a meme!!!!!

--- Memes-Logo/sketch.js
+++ Memes-Logo/sketch.js
@@ -1,12 +1,21 @@
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(1000, 1000);
+}
+
+var lion; // global
+
+function preload() {
+    lion = loadImage("Lion.jpg")
 }
 
 function draw() {
     background(0);
+    
+    image(lion,100, 100)
+    
     fill(255);
     textAlign(CENTER, CENTER);
     textSize(100);
     textFont("Comic Sans MS");
-    text("BLACK GUY!!", width/2, height/2);
+    text("Yurr you talking shit", width/2, 750);
 }
